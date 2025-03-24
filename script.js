48b6a74460df50ae5b670eb91a51769105b3740a
dota031 <115540660+dota031@users.noreply.github.com>
2025-03-25 01:20:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,6 @@ const clickMessage = document.getElementById("click-message");
 
 vinyl.addEventListener("click", function() {
     message.style.display = "block";
-    message.innerHTML = "Gửi em, tình yêu của anh... ❤️";
+    message.innerHTML = "Chúc bạn iu ngày mới zui zẻ nha! Giờ ngủ một giấc dậy là chắc hết khó chịu rùi nhỉ. Những lúc em như này anh chẳng biết phải làm gì để an ủi em. Thui thì mong món quà nhỏ này sẽ làm em cảm thấy vui vẻ iu đời hơn. Dù có chuyện gì xảy ra anh cũng chỉ muốn em biết rằng anh lun iu thương và ủng hộ em ❤️";
     clickMessage.style.display = "none"; // Ẩn dòng chữ hướng dẫn
 });
