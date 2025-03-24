5c16fe0b404e9847bbe3abe5c33dc89d3a279ae7
dota031 <115540660+dota031@users.noreply.github.com>
2025-03-25 00:52:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,5 +12,5 @@ vinyl.addEventListener("click", function() {
         isPlaying = false;
     }
     message.style.display = "block";
-    message.innerHTML = "Gửi em, tình yêu của anh... ❤️";
+    message.innerHTML = "Chúc bạn iu ngày mới zui zẻ nha! Giờ ngủ một giấc dậy là chắc hết khó chịu rùi nhỉ. Những lúc em như này anh chẳng biết phải làm gì để an ủi em. Thui thì mong món quà nhỏ này sẽ làm em cảm thấy vui vẻ iu đời hơn khi thức dậy. Dù có chuyện gì xảy ra anh cũng chỉ muốn em biết rằng anh lun iu thương và ủng hộ em ❤️...";
 });
